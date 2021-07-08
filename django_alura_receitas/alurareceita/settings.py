8618f87e5944344467602255ab1e09503f852867
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2021-07-07 22:00:32 -0300

    Update settings.py

--- django_alura_receitas/alurareceita/settings.py
+++ django_alura_receitas/alurareceita/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'restaurante',
         'USER': 'root',
-        'PASSWORD': 'travaleo123',
+        'PASSWORD': 'abc123',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
